2925bff8c0f0da6a0afe98ee259af226e3378a71
Dhaval Patel <dhavalpatel32768@gmail.com>
2019-07-14 21:33:28 +0530

    Initial Setup

---
+++ README.md
@@ -0,0 +1,36 @@
+## Flux
+
+It is setup made with awesome window manager.
+
+#### Dependencies
+
+* Awesome WM
+* Compton
+* Rofi
+* Alsa
+* Xbacklight
+* Gnome Terminal
+
+#### Features
+
+- [x] Side Bar
+- [x] Tag Bar
+- [x] Start Screen
+- [x] Exit Screen
+- [ ] Task Bar
+- [ ] Minimal Window Decorations
+
+#### Bugs 
+
+- Exit Screen Icons Mismatch
+- Start Screen Colour Contrast
+- Window Rules Causing Dialog Problems
+
+#### Need To Be Improved
+
+- Auto Hide Tag Bar
+- Start Screen Show At Start
+- Brightness Popup When Changed
+- Volume Popup When Changed
+- Minimal Key Interface
+
---
+++ themes/colors.lua
@@ -0,0 +1,23 @@
+local colors = {}
+
+colors.background = "#121228"
+colors.transperent = "#ffffff00"
+colors.text = "#ffffffdd"
+colors.luminent1 = "#24c87b" -- green
+colors.luminent2 = "#2fe1d7" -- teal
+colors.blue_light = "#ABDCFF"
+colors.blue_dark = "#0396FF"
+colors.yellow_light = "#FDEB71"
+colors.yellow_dark = "#F8D800"
+colors.purple_light = "#CE9FFC"
+colors.purple_dark = "#7367F0"
+colors.teal_light = "#90F7EC"
+colors.teal_dark = "#32CCBC"
+colors.green_light = "#81FBB8"
+colors.green_dark = "#28C76F"
+colors.red_light = "#FEB692"
+colors.red_dark = "#EA5455"
+colors.pink_dark = "#F761A1"
+colors.pink_darker = "#8C1BAB"
+return colors
+
